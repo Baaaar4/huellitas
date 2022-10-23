f6911123f868ed9616de0b2fd95b6ffe46be8bfd
Barbara <b.f.castillocontreras@gmail.com>
2022-10-23 15:28:28 -0300

    debug false y manage.py import django

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myservice.settings')
+    django.setup()
     '''if not 'WEBSITE_HOSTNAME' in os.environ:
         print("Loading environment variables for .env file")
         load_dotenv('./.env')
